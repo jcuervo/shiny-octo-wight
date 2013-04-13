40aa29b5e555f915801af6383532a0094b286cb7
Jimmy Cuervo <richard.gonzales@gmail.com>
2013-04-13 10:45:39 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Txtblast::Application.config.secret_token = 'f0584989661ae697d9c1e72e42eb26e4672a2706753cbb74cca53d84d6c3914c80137f4057da137bc8142dd30c1b03b8fa0187ab01a50ee409c81ef2efce6d96'
