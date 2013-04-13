479c296bc43565083ce3b4b9d778065ee3bae356
Julius Francisco <baldrailers@gmail.com>
2013-04-13 14:28:11 +0800

    Adding working yes/no survey

--- lib/tropo_app.rb
+++ lib/tropo_app.rb
@@ -43,7 +43,81 @@ class TropoApp < Sinatra::Base
     survey_id = @v[:result][:actions][:survey_id_input][:value]
     callerid = params[:callerid]
 
+    survey = Survey.find_by_padded_id survey_id
 
+    if survey
+      questions = survey.questions
+      @t.on event: 'continue', next: "/tropo/ask_question.json?question_id=#{questions.first.id}"
+      @t.say "You are now in #{survey.name} survey."
+    else
+      @t.say 'Sorry that is not a valid survey. Goodbye'
+    end
+
+    @t.response
+  end
+
+  post '/ask_question.json' do
+    question = Question.find params[:question_id]
+
+    if question
+      @t.on event: 'continue', next: "/tropo/record_answer.json?question_id=#{question.id}"
+
+      options = {
+        name: 'survey_answer',
+        timeout: 30,
+        required: true,
+        say: {
+          value: question.content
+        },
+        attempts: 3,
+        choices: {
+          value: 'yes , no'
+        }
+      }
+
+      @t.ask options
+
+    else
+      @t.say 'Thank you for using shiny shiny survey. Goodbye.'
+    end
+
+    @t.response
+  end
+
+  post '/record_answer.json' do
+    question = Question.find params[:question_id]
+    caller_id = @v[:session][:from][:id]
+    answer = @v[:result][:actions][:survey_answer][:value]
+
+    survey_answer = question.survey_answers.create(
+      answer: answer.eql?('yes') ? true : false, 
+      caller_id: caller_id
+    )
+
+    next_question = question.next_question(question.survey.id)
+
+    if next_question
+      @t.on event: 'continue', next: "/tropo/record_answer.json?question_id=#{question.id}"
+
+      options = {
+        name: 'survey_answer',
+        timeout: 30,
+        required: true,
+        say: {
+          value: next_question.content
+        },
+        attempts: 3,
+        choices: {
+          value: 'yes , no'
+        }
+      }
+
+      @t.ask options
+    else
+      @t.say 'Thank you for using shiny shiny survey. Goodbye.'
+    end
+
+    @t.response
   end
 
 end
