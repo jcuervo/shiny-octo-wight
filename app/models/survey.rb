950b6f5d626ae30190dbf2e1ca52feb4304cacb8
Jimmy Cuervo <richard.gonzales@gmail.com>
2013-04-13 13:48:29 +0800

    padding ids

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -3,4 +3,8 @@ class Survey < ActiveRecord::Base
   accepts_nested_attributes_for :questions, :reject_if => :all_blank, :allow_destroy => true
   
   attr_accessible :name, :questions_attributes
+  
+  def padded_id
+    "0000#{self.id}"
+  end
 end
